94f63d21d8de77cd823555c3e6a88b335c435636
LauraCMonteroM <lauramontero205@gmail.com>
2023-07-07 12:55:41 -0500

    completes the function of adding new users

--- Burger-Queen-Api-Client/src/assets/admin/EmployeesView.jsx
+++ Burger-Queen-Api-Client/src/assets/admin/EmployeesView.jsx
@@ -1,51 +1,127 @@
 import { useNavigate } from "react-router-dom";
-import { createUser, getData } from "../../services/Users.services";
+import {editDataUser,
+  createUser,
+  getData,
+} from "../../services/Users.services";
 import { useState, useEffect } from "react";
-import './EmployeesView.css';
-import image from '../../imgs/LogoBQ.png';
+import "./EmployeesView.css";
+import image from "../../imgs/LogoBQ.png";
 
 const EmployeesView = () => {
   const [users, setUsers] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false); // Estado para controlar la apertura y cierre del modal
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
+  const logOut = useNavigate();
+  const [editUserId, setEditUserId] = useState(null);
 
   useEffect(() => {
-    getData(users)
-      .then(res => {
-        res.json()
-          .then(setUsers);
-      })
+    getData().then((res) => {
+      res.json().then(setUsers);
+    });
   }, []);
 
-  const closed = useNavigate();
-
   function GotoHome() {
-    closed("/");
+    logOut("/");
   }
 
-  function handleModalOpen() {
+  function handleModalOpen(userId) {
     setIsModalOpen(true);
+    setIsEditModalOpen(false)
+    setEditUserId(userId);
   }
+
+  const handleEditModalOpen = () => {
+    setIsModalOpen(true)
+    setIsEditModalOpen(true);
+  };
 
   function handleModalClose() {
     setIsModalOpen(false);
+    setIsEditModalOpen(false);
   }
 
-  function handleCreateUser() {
-    createUser();
-  }
+  const handleCreateUser = (e) => {
+    e.preventDefault();
+    const newUser = {
+      email: email,
+      password: password,
+      role: role,
+    };
+    createUser(newUser)
+      .then((res) => {
+        console.log(res);
+        newUser.id = res.id;
+        setUsers([...users, newUser]);
+        handleModalClose();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
+  const handleEditUser = (e) => {
+    e.preventDefault();
+    const editedUser = {
+      id: editUserId,
+      email: email,
+      password: password,
+      role: role,
+    };
+    editDataUser(editedUser)
+      .then((res) => {
+        console.log(res);
+        const updatedUsers = users.map((user) => {
+          if (user.id === editUserId) {
+            return editedUser;
+          } else {
+            return user;
+          }
+        });
+        setUsers(updatedUsers);
+        handleModalClose();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleEmailChange = (e) => {
+    const newEmail = e.target.value;
+    setEmail(newEmail);
+  };
+
+  const handlePasswordChange = (e) => {
+    const newPassword = e.target.value;
+    setPassword(newPassword);
+  };
+
+  const handleRoleChange = (e) => {
+    const newRole = e.target.value;
+    setRole(newRole);
+  };
+  
+  
   return (
-    <div id='adminEmployeesContainer'>
+    <div id="adminEmployeesContainer">
       <nav>
-        <img src={image} className='navLogo' alt='Burger Queen Logo'/>
-        <i className="bi bi-box-arrow-right" id='logOut' onClick={GotoHome}></i>
+        <img src={image} className="navLogo" alt="Burger Queen Logo" />
+        <i className="bi bi-box-arrow-right" id="logOut" onClick={GotoHome}></i>
       </nav>
       <section className="prodEmplButtons">
-        <button type="button" className="employeesButton">Empleados</button>
-        <button type="button" className="productsButton">Productos</button>
+        <button type="button" className="employeesButton">
+          Empleados
+        </button>
+        <button type="button" className="productsButton">
+          Productos
+        </button>
       </section>
       <section id="btnAdd">
-        <button type="button" className="addButton" onClick={handleModalOpen}>Añadir Empleado</button>
+        <button type="button" className="addButton" onClick={() => handleModalOpen()}>
+          Añadir Empleado
+        </button>
       </section>
       <section className="tableSection">
         <table className="employeesTable">
@@ -56,29 +132,83 @@ const EmployeesView = () => {
           </thead>
           <tbody>
             {users.map((user) => {
-                return (
-                  <tr key={user.id}>
-                    <td>{user.email}</td>
-                    <td>{user.role}</td>
-                    <td><i className="bi bi-pencil-fill"></i></td>
-                    <td><i className="bi bi-trash3-fill"></i></td>
-                  </tr>
-                );
-              })}
+              return (
+                <tr key={user.id}>
+                  <td>{user.email}</td>
+                  <td>{user.role}</td>
+                  <td>
+                    <i className="bi bi-pencil-fill" onClick={() => handleEditModalOpen(user.id)}></i>
+                  </td>
+                  <td>
+                    <i className="bi bi-trash3-fill"></i>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </section>
       {isModalOpen && (
         <div className="modal">
-          {/* Contenido del modal */}
           <div className="modal-content">
-            <span className="close" onClick={handleModalClose}>&times;</span>
-            {/* Aquí puedes agregar los inputs y botones necesarios */}
-            <input type="text" placeholder="Email" />
-            <input type="password" placeholder="Contraseña" />
-            <input type="text" placeholder="Rol" />
-            <button type="button" onClick={handleModalClose}>Cerrar</button>
-            <button type="submit"onClick={handleCreateUser}>Guardar cambios</button>
+            <i
+              className="bi bi-x-square-fill closeButton"
+              onClick={handleModalClose}
+            ></i>
+            {isEditModalOpen ? (
+              <form onSubmit={handleEditUser}>
+                <h2>Editar usuario</h2>
+                <label className="modalLabel">Correo Electrónico</label>
+                <input
+                  type="text"
+                  value={email}
+                  onChange={handleEmailChange}
+                />
+                <label className="modalLabel">Contraseña</label>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
+                <label className="modalLabel">Rol</label>
+                <input
+                  type="text"
+                  value={role}
+                  onChange={handleRoleChange}
+                />
+                <button type="submit" className="modalButton">
+                  Guardar
+                </button>
+              </form>
+            ) : (
+              <form onSubmit={handleCreateUser}>
+                <h2>Añadir usuario</h2>
+                <label className="modalLabel">Correo Electrónico</label>
+                <input
+                  type="text"
+                  placeholder="ejemplo@ejemplo.com"
+                  value={email}
+                  onChange={handleEmailChange}
+                />
+                <label className="modalLabel">Contraseña</label>
+                <input
+                  type="password"
+                  placeholder="****************"
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
+                <label className="modalLabel">Rol</label>
+                <input
+                  type="text"
+                  placeholder="Waiter/Chef/Admin"
+                  value={role}
+                  onChange={handleRoleChange}
+                />
+                <button type="submit" className="modalButton">
+                  Añadir
+                </button>
+              </form>
+            )}
           </div>
         </div>
       )}
